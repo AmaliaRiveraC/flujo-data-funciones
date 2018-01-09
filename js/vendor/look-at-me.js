0dcc66c5df8c4b3ec36bca18044380eda393c205
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2018-01-09 15:53:33 +0000

    add elements and events

--- js/vendor/look-at-me.js
+++ js/vendor/look-at-me.js
@@ -1,6 +1,11 @@
 +function () {
     // Traer los elementos que se necesitan del HTML
-   
+   var addBoton = document.getElementById('agregar');
+   var showBoton = document.getElementById('mostrar');
+   var searchBoton = document.getElementById('buscar');
+   var topTechBoton = document.getElementById('top-tecnico');
+   var topHSEBoton = document.getElementById('top-hse');
+   var result = document.getElementById('contenedor-estudiantes');
     
     // Evento Click - Agregar
     var eventoAgregar = function (e) {
@@ -25,18 +30,30 @@
     
     var eventoTopTecnico = function (e) {
         e.preventDefault();
+        console.log('hola');
         var estudiantes = obtenerListaEstudiantes();
         var estudiantesTopTecnico = topTecnico(estudiantes);
         resultado.innerHTML = mostrarLista(estudiantesTopTecnico);
     };
     
+
+    
+    
     var eventoTopHSE = function (e) {
         e.preventDefault();
+        console.log('Adios');
         var estudiantes = obtenerListaEstudiantes();
         var estudiantesTopHSE = topHSE(estudiantes);
         resultado.innerHTML = mostrarLista(estudiantesTopHSE);
     };
+
+    
     
     // Agregar eventos que correspondan
+    addBoton.addEventListener('click', eventoAgregar);
+    showBoton.addEventListener('click', eventoMostrar);
+    searchBoton.addEventListener('click', eventoBuscar);
+    topHSEBoton.addEventListener('click', eventoTopHSE);
+    topTechBoton.addEventListener('click', eventoTopTecnico);
     
 }();
